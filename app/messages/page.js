5f5345bada89832c2a0253881beeba6166ac7c2b
Benny <benedetto.pana@gmail.com>
2024-11-16 11:41:46 +0100

    various test width NextJS caching

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,12 +1,20 @@
-import Messages from '@/components/messages';
+import { unstable_noStore as noStore } from "next/cache";
+
+import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
+
+// export const revalidate = 5;
+// export const dynamic = 'force-dynamic';
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
+  // noStore();
+
+  // const response = await fetch("http://localhost:8080/messages", {
+  //   next: { tags: ["msg"] },
+  // });
+  // const messages = await response.json();
+
+  const messages = await getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
